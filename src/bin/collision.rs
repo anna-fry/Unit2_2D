4bf8e328418edef3fd55eae5ec92137f3178706a
atfry <afry@hmc.edu>
2021-02-22 10:54:55 -0800

    tiles started

---
+++ src/bin/collision.rs
@@ -0,0 +1,1 @@
+// TODO: Compare pos of obstacles and the player from the state and change velocity/health based on collision
--- src/bin/sprite.rs
+++ src/bin/sprite.rs
@@ -3,7 +3,7 @@ use crate::types::{Rect, Vec2i};
 use std::rc::Rc;
 
 pub struct Sprite {
-    image: Rc<Texture>,
+    pub image: Rc<Texture>,
     pub frame: Rect, // Maybe better to use a type that can't have a negative origin
     // Or use =animation:Animation= instead of a frame field
     pub position: Vec2i,
@@ -16,5 +16,18 @@ impl Sprite {
             frame,
             position,
         }
+    }
+}
+
+pub trait DrawSpriteExt {
+    fn draw_sprite(&mut self, s: &Sprite);
+}
+
+use crate::screen::Screen;
+impl<'fb> DrawSpriteExt for Screen<'fb> {
+    fn draw_sprite(&mut self, s: &Sprite) {
+        // This works because we're only using a public method of Screen here,
+        // and the private fields of sprite are visible inside this module
+        self.bitblt(&s.image, s.frame, s.position);
     }
 }
